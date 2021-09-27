4ce6fba66ca8b139e24d4250f7e0d7cd46cf26c0
Tomasz Korab <tomaszkorab@MacBook-Pro-Tomasz.local>
2021-09-27 22:02:04 +0200

    Schemes: AppOnline, AppOffline, Networking, NetworkingTests

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -6,20 +6,11 @@
 //
 
 import SwiftUI
-import CurrencyPreview
 
 struct ContentView: View {
+
     var body: some View {
-        NavigationView {
-            GeometryReader { proxy in
-                VStack {
-                    CurrencyPreview(values: [1.11, 1.21, 0.70, 1.2, 0.96, 1.5, 1.45, 1.2, 1.21, ])
-                        .frame(width: proxy.size.width, height: proxy.size.height * 0.3)
-                    Spacer()
-                }
-            }
-            .navigationTitle("USD")
-        }
+        Text("Hello")
     }
 }
 
